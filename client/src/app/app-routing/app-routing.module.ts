709d702f2cee211b2fff6d63eeef4bb12dfee81d
Alex Chaliy <alex.chaliy.42@gmail.com>
2018-07-09 21:01:00 +0300

    client: app routing

---
+++ client/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,23 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MainPageComponent } from '../main-page/main-page/main-page.component';
+
+const routes: Routes = [
+		{ path: '', redirectTo: '/main', pathMatch: 'full' },
+		{ path: 'main', component: MainPageComponent },
+];
+
+@NgModule({
+	imports: [
+		CommonModule,
+		RouterModule.forRoot(routes)
+	],
+	exports: [
+		RouterModule
+	],
+	declarations: []
+})
+export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MainPageModule } from './main-page/main-page.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+
+    MainPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
